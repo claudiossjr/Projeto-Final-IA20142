d3b37146fd083d620bcca6b689e948632b0d133d
dinhujr <c.souza0804@gmail.com>
2014-11-20 21:43:12 -0200

    numero de tuplas

--- Apriori_BDConnection.py
+++ Apriori_BDConnection.py
@@ -1,3 +1,5 @@
+from PIL.ImImagePlugin import number
+
 __author__ = 'joaolucas'
 
 
@@ -9,20 +11,22 @@ import MySQLdb
 print "Insert hostname"
 host = "localhost"#raw_input()
 print "Insert Username"
-username = "joao" #raw_input()
+username = "root" #raw_input()
 print "Insert your password"
-password = "1234" #raw_input()
+password = "423123" #raw_input()
 print "Insert Database"
 database = "sakila" #raw_input()
 
 db = MySQLdb.connect(host,username,password,database)
 cursor = db.cursor()
 
+numberOfRows = 0
 
 def checkNumberOfRows():
 
     cursor.execute("SELECT COUNT(*) FROM rental")
     numberOfRows = cursor.fetchone()
+    numberOfRows = numberOfRows[0]
     print numberOfRows
 
 
