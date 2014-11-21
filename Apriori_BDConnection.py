939bc289d19634671e31f37ceb8e6d85c6b85615
joaorulff <jlrulff@gmail.com>
2014-11-21 12:59:47 -0200

    calculating the L1

--- Apriori_BDConnection.py
+++ Apriori_BDConnection.py
@@ -5,47 +5,58 @@ __author__ = 'joaolucas'
 
 import MySQLdb
 
-
-
-
-print "Insert hostname"
-host = "localhost"#raw_input()
-print "Insert Username"
-username = "joao" #raw_input()
-print "Insert your password"
-password = "1234" #raw_input()
-print "Insert Database"
-database = "teste" #raw_input()
-
-db = MySQLdb.connect(host, username, password, database)
-cursor = db.cursor()
-
-
 numberOfRows = 0
+suport = 0.4
 
-def checkNumberOfRows():
+def dbConnection():
+    print "Insert hostname"
+    host = "localhost"#raw_input()
+    print "Insert Username"
+    username = "joao" #raw_input()
+    print "Insert your password"
+    password = "1234" #raw_input()
+    print "Insert Database"
+    database = "teste" #raw_input()
 
-    cursor.execute("SELECT COUNT(*) FROM Compra")
-    numberOfRows = cursor.fetchone()
+    db = MySQLdb.connect(host, username, password, database)
+    cursor = db.cursor()
+    checkTransactions(cursor)
 
-    numberOfRowsToInt = int(numberOfRows[0])
 
-    qtdList=[0 for i in range(numberOfRowsToInt+1)]
+def checkTransactions(cursor):
+
+    cursor.execute("SELECT COUNT(DISTINCT idCliente) FROM Compra")
+    numberOfTransactions = int(cursor.fetchone()[0])
+
+    transactions = [[] for i in range(numberOfTransactions+1)]
 
     cursor.execute("SELECT * FROM Compra")
-    results = cursor.fetchall()
+    auxtransactions = cursor.fetchall()
 
-    for row in results:
-        idproduto = row[0]
-        idcliente = row[1]
+    for tuple in auxtransactions:
+       transactions[int(tuple[1])].append(int(tuple[0]))
 
-        qtdList[idproduto] = qtdList[idproduto] + 1
+    buildC1(transactions, cursor)
+
+def buildC1(transactions, cursor):
+
+    C1 = {}
+
+    for ID in transactions:
+        for product in ID:
+            if not C1.has_key(product): C1[product] = 1
+            else: C1[product] += 1
+
+    L1 = {}
+
+    for item in C1:
+        if (float(C1[item]) / float(len(transactions))) > suport:
+            L1[item] = C1[item]
+            cursor.execute("SELECT nome FROM Produtos WHERE idProdutos = %s" % (item) )
+            productName = cursor.fetchall()
+
+            for aux in productName:
+                print "%s -----> Suporte: %s " % (aux[0], float(C1[item]) / float(len(transactions)))
 
 
-
-    for index in range(len(qtdList)):
-        print ("i %s quantidade %s " % (index, qtdList[index]))
-
-
-
-checkNumberOfRows()
+dbConnection()
