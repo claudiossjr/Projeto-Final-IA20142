cc2cf0f8f99ca1337bc00383a6ea977fad974b6e
joaorulff <jlrulff@gmail.com>
2014-11-23 11:06:40 -0200

    SAVE TUPLES IN BD

--- Apriori_BDConnection.py
+++ Apriori_BDConnection.py
@@ -36,6 +36,11 @@ def checkTransactions(cursor):
     for tuple in auxtransactions:
        transactions[int(tuple[1])].append(int(tuple[0]))
 
+    # print de cada transacao
+    for item in transactions:
+        print item
+
+
     buildC1(transactions, cursor)
 
 def buildC1(transactions, cursor):
@@ -47,6 +52,11 @@ def buildC1(transactions, cursor):
             if not C1.has_key(product): C1[product] = 1
             else: C1[product] += 1
 
+
+    # print do C1. {idProduto, Quantidade}
+    for item in C1:
+        print " Item: %s Quantidade %s " % (item ,C1[item])
+
     L1 = {}
 
     for item in C1:
@@ -57,6 +67,48 @@ def buildC1(transactions, cursor):
 
             for aux in productName:
                 print "%s -----> Suporte: %s " % (aux[0], float(C1[item]) / float(len(transactions)))
+
+    joinOperation(L1,transactions)
+
+def joinOperation(L1,transactions):
+
+    LK = {}
+
+    auxList = L1.keys()
+    newListofTuples = list();
+
+    for i in range(0, len(auxList)):
+        for j in range(i+1, len(auxList)):
+            newListofTuples.append([auxList[i],auxList[j]])
+
+    for item in newListofTuples:
+        print item
+
+    print "___________________"
+    print newListofTuples [0][0]
+    print newListofTuples [0][1]
+    print newListofTuples [1][0]
+    print newListofTuples [1][1]
+    print newListofTuples [2][0]
+    print newListofTuples [2][1]
+
+    saveRules(len(newListofTuples[0]))
+
+def saveRules(K):
+
+    sql = "CREATE TABLE L%s (id INT, element INT)" %(K)
+
+    print sql
+
+
+def checkSubset(transactions, tupleList):
+
+    CK = {}
+    i = 0
+    for ID in transactions:
+        for item in ID:
+            print ""
+    print tupleList[0][0]
 
 
 dbConnection()
